c27e9957786982a468212187ba7f8fb63230c508
KimJanson <you@example.com>
2023-01-26 10:47:49 +0100

    first draft

---
+++ Versuch.py
@@ -0,0 +1,9 @@
+x = "aller zeiten."
+
+def myfunc():
+  x = "der beste."
+  print("Ronaldo ist " + x)
+  
+myfunc()
+
+print("Der beste Fußballer " + x)
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-eingabe = input("Eingabe: ")
+eingabe = input("Ihre Eingabe: ")
 
-eingaben = eingabe.split("+, -, *, /: ")
+eingaben = eingabe.split ("+")
+eingaben1 = eingabe.split("-")
 
 zahl1 = int(eingaben[0]) 
-zahl2 = int(eingaben[1]) 
+zahl2 = int(eingaben[1])  
 
 ergebnis = zahl1 + zahl2
 print("Das Ergebnis ist " + str(ergebnis))
