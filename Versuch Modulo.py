c2c84f90307c31b73b0f90ebb2277211f892be66
KimJanson <you@example.com>
2023-02-03 13:28:34 +0100

    last draft

--- Versuch Modulo.py
+++
@@ -1,5 +0,0 @@
-import math
-math.fmod(12.5, 5.5)
-
-
-math.fmod(8.5, 2.5)
--- test2.py
+++ test2.py
@@ -3,13 +3,23 @@ import re
 def rechner():
     eingabe = input("Ihre Eingabe: ")
 
-x = re.search("^\d+[\*\+\-/]\d+$", txt)
-    if "+", "-", "*","/" in eingabe:
-        eingaben = eingabe.split ("+", "-", "*", "/")
-    else:
-        print("Ungültige Eingabe")
+    match = re.search("^\d+[\*\+\-/]\d+$", eingabe)
+
+    if not match:
+        print("!!!Ungültiges Zeichen!!!")
+        return
         
+    if "+" in eingabe:
+        eingaben = eingabe.split ("+")
+    if "-" in eingabe:
+        eingaben = eingabe.split ("-")
+    if "*" in eingabe:
+        eingaben = eingabe.split ("*")   
+    if "/" in eingabe:
+        eingaben = eingabe.split ("/")
+
     zahl1 = int(eingaben[0]) 
+
     zahl2 = int(eingaben[1]) 
  
     if "+" in eingabe:
@@ -24,15 +34,10 @@ x = re.search("^\d+[\*\+\-/]\d+$", txt)
     print("Das Ergebnis ist " + str(ergebnis))
 
 while True:
-    x = re.search("^\d+[\*\+\-/]\d+$", eingaben)
-    if x:
-        continue
-    else:
-        print("Ungültige Eigabe")
-
     rechner()
     eingaben1 = input("Weitere Eingabe? (ja/nein): ")
     if eingaben1 == "nein":
         break
-else:
-    print("Ungültiges Zeichen")
+    elif eingaben1 != "ja":
+        print("!!!Ungültiges Zeichen!!!")
+        break
